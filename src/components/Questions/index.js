df81f53ca68dfa983bf26f52c9031b0e73f17468
“sabrina-goncalves-de-almeida” <sabrina.goncalves@dcx.ufpb.br>
2025-07-28 08:19:42 -0300

    feat(#2): componentes da tela de questões, estáticos.

---
+++ src/components/Questions/index.js
@@ -0,0 +1,31 @@
+import styles from "./Questions.module.css";
+
+function Questions(){
+    return(
+        <>
+            <div className={styles.questionsContainer}>
+                <div className={styles.question}>
+                    a) 
+                </div>
+
+                <div className={styles.question}>
+                   b)  
+                </div>
+
+                <div className={styles.question}>
+                   c)  
+                </div>
+
+                <div className={styles.question}>
+                   d)  
+                </div>
+
+                <div className={styles.question}>
+                   e)  
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Questions;
